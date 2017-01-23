376e157663624a10f971710de3aa1bcc20c97056
Brian Harris <sendbrianemail@gmail.com>
2017-01-22 22:01:30 -0500

    sending messages with file path and selected file are working.

--- src/main/java/org/sitenv/directtransportmessagesender/controllers/MessageSenderController.java
+++ src/main/java/org/sitenv/directtransportmessagesender/controllers/MessageSenderController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Created by Brian on 1/19/2017.
@@ -20,9 +21,15 @@ public class MessageSenderController {
         this.messageSenderService = messageSenderService;
     }
 
-    @RequestMapping(value = "/sendprecannedccda", method = RequestMethod.POST)
-    public MessageSenderResult sendDirectMessage(@RequestParam(value = "toAddress", required = true) String toAddress,
-                                                 @RequestParam(value = "ccdaFilePath", required = true) String ccdaFilePath){
-        return messageSenderService.sendMessage(toAddress, ccdaFilePath);
+    @RequestMapping(value = "/sendmessagewithattachmentfilepath", method = RequestMethod.POST)
+    public MessageSenderResult sendMessageWithAttachment(@RequestParam(value = "toAddress", required = true) String toAddress,
+                                                 @RequestParam(value = "attachmentFilePath", required = true) String attachmentFilePath){
+        return messageSenderService.sendMessageWithAttachment(toAddress, attachmentFilePath);
+    }
+
+    @RequestMapping(value = "/sendmessagewithattachmentfile", headers = "content-type=multipart/*", method = RequestMethod.POST)
+    public MessageSenderResult sendMessageWithAttachment(@RequestParam(value = "toAddress", required = true) String toAddress,
+                                                 @RequestParam(value = "attachment", required = true) MultipartFile attachment){
+        return messageSenderService.sendMessageWithAttachment(toAddress, attachment);
     }
 }
--- src/main/java/org/sitenv/directtransportmessagesender/services/MessageSenderService.java
+++ src/main/java/org/sitenv/directtransportmessagesender/services/MessageSenderService.java
@@ -4,14 +4,18 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.sitenv.directtransportmessagesender.dto.MessageSenderResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamSource;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 
 /**
  * Created by Brian on 1/19/2017.
@@ -29,30 +33,54 @@ public class MessageSenderService {
         this.javaMailSender = mailSender;
     }
 
-    public MessageSenderResult sendMessage(String to, String selectedFile){
+    public MessageSenderResult sendMessageWithAttachment(String to, String attachmentFilePath){
         MimeMessage message = javaMailSender.createMimeMessage();
         MessageSenderResult messageSenderResult = new MessageSenderResult();
         messageSenderResult.setSuccess(false);
         try{
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setFrom(from);
-            helper.setTo(to);
-            helper.setSubject(EMAIL_SUBJECT_TEXT);
-            helper.setText(EMAIL_BODY_TEXT);
-
-            FileSystemResource file = new FileSystemResource(selectedFile);
+            MimeMessageHelper helper = configureMessageHelper(message, to);
+            FileSystemResource file = new FileSystemResource(attachmentFilePath);
             helper.addAttachment(file.getFilename(), file);
             javaMailSender.send(message);
-            messageSenderResult.setSuccess(true);
-            messageSenderResult.setMessage("Message has been sent to the host successfully - no more information is available. Please check the inbox of " + to + " to ensure delivery.");
-        }catch (MessagingException e) {
-            messageSenderResult.setSuccess(false);
-            messageSenderResult.setMessage("Message not sent with the following error: " + ExceptionUtils.getStackTrace(e));
-        }catch (MailSendException e){
-            messageSenderResult.setSuccess(false);
-            messageSenderResult.setMessage("Could not connect to the mail server with the following error : " + ExceptionUtils.getStackTrace(e));
+            setSuccessfulMessageSenderResult(messageSenderResult);
+        }catch (MessagingException | MailSendException e) {
+            setUnsuccessfulMessageSenderResult(messageSenderResult, ExceptionUtils.getStackTrace(e));
         }
-
         return messageSenderResult;
+    }
+
+    public MessageSenderResult sendMessageWithAttachment(String to, MultipartFile attachmentFile){
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MessageSenderResult messageSenderResult = new MessageSenderResult();
+        messageSenderResult.setSuccess(false);
+        try{
+            MimeMessageHelper helper = configureMessageHelper(message, to);
+            InputStreamSource inputStreamSource = new ByteArrayResource(attachmentFile.getBytes());
+            helper.addAttachment(attachmentFile.getOriginalFilename(), inputStreamSource);
+            javaMailSender.send(message);
+            setSuccessfulMessageSenderResult(messageSenderResult);
+        }catch (MessagingException | MailSendException | IOException e) {
+            setUnsuccessfulMessageSenderResult(messageSenderResult, ExceptionUtils.getStackTrace(e));
+        }
+        return messageSenderResult;
+    }
+
+    private MimeMessageHelper configureMessageHelper(MimeMessage message, String to) throws MessagingException {
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(EMAIL_SUBJECT_TEXT);
+        helper.setText(EMAIL_BODY_TEXT);
+        return helper;
+    }
+
+    private void setSuccessfulMessageSenderResult(MessageSenderResult messageSenderResult){
+        messageSenderResult.setSuccess(true);
+        messageSenderResult.setMessage("Message has been sent to the host successfully - no more information is available. Please check the recipient's inbox to ensure delivery.");
+    }
+
+    private void setUnsuccessfulMessageSenderResult(MessageSenderResult messageSenderResult, String error){
+        messageSenderResult.setSuccess(false);
+        messageSenderResult.setMessage("Message not sent with the following error: " + error);
     }
 }
